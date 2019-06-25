4d0eeddec3b03f75e98f7393ed5d45d256bd7eb1
Ruris <969954321@qq.com>
2019-06-25 13:31:34 +0800

    add listbox style
    add listbox style

---
+++ Panuon.UI.Silver/Helpers/Control/ListBoxHelper.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Threading;
+namespace Panuon.UI.Silver
+{
+    public class ListBoxHelper
+    {
+        #region ItemHeight
+        public static double GetItemHeight(DependencyObject obj)
+        {
+            return (double)obj.GetValue(ItemHeightProperty);
+        }
+
+        public static void SetItemHeight(DependencyObject obj, double value)
+        {
+            obj.SetValue(ItemHeightProperty, value);
+        }
+
+        public static readonly DependencyProperty ItemHeightProperty =
+            DependencyProperty.RegisterAttached("ItemHeight", typeof(double), typeof(ListBoxHelper));
+        #endregion
+
+        #region HoverBrush
+        public static Brush GetHoverBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(HoverBrushProperty);
+        }
+
+        public static void SetHoverBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(HoverBrushProperty, value);
+        }
+
+        public static readonly DependencyProperty HoverBrushProperty =
+            DependencyProperty.RegisterAttached("HoverBrush", typeof(Brush), typeof(ListBoxHelper));
+        #endregion
+
+        #region SelectedBrush
+        public static Brush GetSelectedBrush(DependencyObject obj)
+        {
+            return (Brush)obj.GetValue(SelectedBrushProperty);
+        }
+
+        public static void SetSelectedBrush(DependencyObject obj, Brush value)
+        {
+            obj.SetValue(SelectedBrushProperty, value);
+        }
+
+        public static readonly DependencyProperty SelectedBrushProperty =
+            DependencyProperty.RegisterAttached("SelectedBrush", typeof(Brush), typeof(ListBoxHelper));
+        #endregion
+    }
+}
