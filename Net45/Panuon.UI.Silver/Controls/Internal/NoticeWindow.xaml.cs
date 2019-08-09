ac2b72bb475dd3747d1f68e7620365c6551bdabb
Ruris <969954321@qq.com>
2019-08-09 18:26:23 +0800

    update

---
+++ Net45/Panuon.UI.Silver/Controls/Internal/NoticeWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace Panuon.UI.Silver.Controls.Internal
+{
+    /// <summary>
+    /// NoticeWindow.xaml 的交互逻辑
+    /// </summary>
+    internal partial class NoticeWindow : Window
+    {
+        public NoticeWindow()
+        {
+            InitializeComponent();
+            Instance = this;
+
+            Closing += NoticeWindow_Closing;
+        }
+
+        
+        #region Property
+        public static NoticeWindow Instance { get; set; }
+        #endregion
+
+        #region EventHandler
+        private void NoticeWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            Instance = null;
+        }
+        #endregion
+
+        #region Calling Method
+        public void AddNotice()
+        {
+
+        }
+        #endregion
+    }
+}
---
+++ Net45/Panuon.UI.Silver/Controls/Notice.cs
@@ -0,0 +1,32 @@
+﻿using Panuon.UI.Silver.Controls.Internal;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Panuon.UI.Silver.Controls
+{
+    public class Notice
+    {
+        public static void Show(string message , string title)
+        {
+            CallNoticeWindow(message, title, null);
+        }
+
+        public static void Show(string message, string title, double durationSeconds = 3)
+        {
+            CallNoticeWindow(message, title, durationSeconds);
+        }
+
+        private static void CallNoticeWindow(string message, string title, double? durationSeconds)
+        {
+            if (NoticeWindow.Instance == null)
+            {
+                var window = new NoticeWindow();
+                window.Show();
+            }
+            NoticeWindow.Instance.AddNotice();
+        }
+    }
+}
