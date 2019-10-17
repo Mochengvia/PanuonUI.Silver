a4316f38f4c39301bdda7584cb755ba6aeef37ef
Ruris <969954321@qq.com>
2019-10-17 11:00:21 +0800

    v1.0.8.3 fix some problems

--- Net45/Panuon.UI.Silver/Properties/AssemblyInfo.cs
+++ Net45/Panuon.UI.Silver/Properties/AssemblyInfo.cs
@@ -52,7 +52,7 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.8.2")]
-[assembly: AssemblyFileVersion("1.0.8.2")]
+[assembly: AssemblyVersion("1.0.8.3")]
+[assembly: AssemblyFileVersion("1.0.8.3")]
 [assembly: Guid("16dbb0c9-f9b0-4bcf-a1f6-cb740041699e")]
 
