183880efff3ae9adf926ebd5ef7e54d40bc7dafe
REA ZHUANG <reazhuang@outlook.com>
2019-10-26 09:57:41 +0800

    Support publih as NetCore3.0(WPF) library Build success, test a little.

---
+++ NetCore3.0/Panuon.UI.Silver/Models/NotifyPropertyChanged.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace Panuon.UI.Silver.Core
+{
+    public class PropertyChangedBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
