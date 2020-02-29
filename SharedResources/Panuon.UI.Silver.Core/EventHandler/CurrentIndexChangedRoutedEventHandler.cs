f1e16c7fd171271db4a7e7e837baf427d2d8fb41
末城via <969954321@qq.com>
2020-02-29 14:40:56 +0800

    Stash

---
+++ SharedResources/Panuon.UI.Silver.Core/EventHandler/CurrentIndexChangedRoutedEventHandler.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+
+namespace Panuon.UI.Silver.Core
+{
+       public class CurrentIndexChangedRoutedEventArgs : RoutedEventArgs
+    {
+        public CurrentIndexChangedRoutedEventArgs(RoutedEvent routedEvent) : base(routedEvent) 
+        {
+
+        }
+
+        public int CurrentIndex { get; set; }
+    }
+
+    public delegate void CurrentIndexChangedRoutedEventHandler(object sender, CurrentIndexChangedRoutedEventArgs e);
+
+}
